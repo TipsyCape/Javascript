c233ed72333573c0def349d7ffad0aa9268c9c39
TipsyCape <jmhubka@gmail.com>
2022-08-30 10:33:05 -0500

    house built with p5

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//javascript comments are just like c
+
+//vars
+x=1;
+let s = 'this is a string';
+console.log(s);
+console.log('This is how you reference the value of a variable: ${s}');
+
+function setup() {
+    createCanvas(400, 400);   //width height. creates a canvas with origin(0,0) being in the top left of the site
+}
+
+function draw() {
+    background(120, 200, 300);
+    fill(48, 194, 43);    //(R, G, B) on a scale from 0-255. you can also do background(0-255) for grayscale
+    rect(0, 300, 400, 100);   //grass
+    fill (255, 0, 0);       
+    rect(100, 100, 200, 200); //house
+    fill(99, 68, 24);
+    triangle(200, 0, 100, 100, 300, 100); //roof
+    rect(150, 200, 50, 100);
+    circle(160, 260, 10);
+    fill(255, 255, 255);
+    rect(230, 140, 50, 50);
+    line(230, 140, 230, 190);
+    
+}
